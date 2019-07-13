50db03093ffa574ca4ed77f54a76d7c409877c18
Liren <liren90@gmail.com>
2019-07-13 13:13:08 +0800

    Gatsby plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Layout } from '../components/layout'
 import { Link } from 'gatsby'
-import '../app.css'
+import '../app.scss'
 
 const HomePage = () => {
   return (
